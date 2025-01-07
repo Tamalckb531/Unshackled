bac94ab6853acbac7a5697a36d3feb1fe44c4463
Tamal <ckbtamaldipnew@gmail.com>
2025-01-07 21:55:18 +0600

    Editor feature start

--- frontend/src/app/editor/page.tsx
+++ frontend/src/app/editor/page.tsx
@@ -2,6 +2,8 @@ import NotePicker from "@/components/NotePicker";
 import React from "react";
 
 const page = () => {
+  //? Time to build the editor
+
   return (
     <div className=" bg-white h-[89vh] text-4xl text-black text-center">
       <NotePicker />
