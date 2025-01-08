cef9b5ba5fdfbc284a1006b0d1b5cf9c7b699289
Tamal <ckbtamaldipnew@gmail.com>
2025-01-08 20:16:17 +0600

    Editor feature -> frontend -> delete the previous things

--- frontend/src/app/editor/page.tsx
+++ frontend/src/app/editor/page.tsx
@@ -1,13 +1,10 @@
-import NotePicker from "@/components/NotePicker";
 import React from "react";
 
 const page = () => {
   //? Time to build the editor
 
   return (
-    <div className=" bg-white h-[89vh] text-4xl text-black text-center">
-      <NotePicker />
-    </div>
+    <div className=" bg-white h-[89vh] text-4xl text-black text-center">hi</div>
   );
 };
 
