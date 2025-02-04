8837ac61b51028860fe31502c5803cb9badf18ed
Tamal <ckbtamaldipnew@gmail.com>
2025-02-04 11:52:42 +0600

    Editor collboration feature -> backend -> add ws backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,16 +2,23 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
+import http from 'http';
+import { WebSocketServer, WebSocket } from 'ws';
 import authRoute from './routes/auth.route'
 import userRoute from './routes/user.route'
 import newsRoute from './routes/news.route'
 import commentRoute from './routes/comment.route'
-import verifyToken from './utils/verifyToken';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
+
+//? server for handling both express and websocket
+const server = http.createServer(app);
 
 // Middleware to parse JSON
 app.use(cors({
@@ -35,6 +42,93 @@ app.get('/test', (req: Request, res: Response) => {
     status:"healthy"
   });
 });
+
+//! --------- Web Socket server ---------
+const wss = new WebSocketServer({ server });
+
+interface JwtPayload{
+  id: string;
+}
+
+const clients = new Map<string, { ws: WebSocket, userId: string }>();
+
+wss.on('connection', async (ws, req) => {
+  const token = new URL(req.url || '', `http://${req.headers.host}`).searchParams.get('token');
+  const secretKey = process.env.JWT_SECRET_KEY;
+
+  if (!token || !secretKey) {
+    ws.close();
+    return;
+  }
+
+  jwt.verify(token, secretKey, async (error, decode) => {
+    if (error) {
+      ws.close();
+      return;
+    }
+
+    const { id: userId } = decode as JwtPayload;
+
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: {
+          id: true,
+          userName: true,
+          firstName: true,
+          lastName: true,
+          photoURL: true
+        }
+      });
+
+      if (!user) {
+        console.log("No user found");
+        ws.close();
+        return;
+      }
+
+      clients.set(userId, { ws, userId });
+      console.log(`User ${user.firstName} connected to WebSocket`);
+
+      ws.on('message', async (msg) => {
+        try {
+          const { type, collaborators } = JSON.parse(msg.toString());
+  
+          if (type === "send_invitation") {
+            for (const collabId of collaborators) {
+              const collabWs = clients.get(collabId);
+              if (collabWs) {
+                collabWs.ws.send(JSON.stringify({
+                  type: "invitation",
+                  from: user.userName,
+                  firstName: user.firstName,
+                  lastName: user.lastName,
+                  photoURL: user.photoURL
+                }));
+              }
+            }
+          }
+        } catch (error) {
+          console.error("Error in message handling:", error);
+          clients.delete(userId);
+          ws.close();
+        }
+      });
+
+      ws.on('close', () => {
+        clients.delete(userId);
+        console.log(`User ${user.firstName} disconnected`);
+      })
+      
+    } catch (error) {
+      console.error("Failed in database operation", error);
+      ws.close();
+    }
+  })
+
+})
+
+//! --------- Web Socket server ---------
 
 // Start the server
 app.listen(port, () => {
