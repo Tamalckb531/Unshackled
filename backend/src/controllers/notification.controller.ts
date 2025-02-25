ee36c1d3cdcd709d252124b86d38647dbb3fe37f
Tamal <ckbtamaldipnew@gmail.com>
2025-02-25 17:26:54 +0600

    Notification feature -> backend -> add getUnCheckedCount controller

--- backend/src/controllers/notification.controller.ts
+++ backend/src/controllers/notification.controller.ts
@@ -21,17 +21,17 @@ export const getNotifications = async (req: Request, res: Response, next: NextFu
     }
 }
 
-export const getCheckedCount = async(req: Request, res: Response, next: NextFunction) => {
+export const getUnCheckedCount = async(req: Request, res: Response, next: NextFunction) => {
     const userId = req.user?.id;
 
     try {
-        const checkedCount: number = await prisma.notification.count({
+        const uncheckedCount: number = await prisma.notification.count({
             where: {
                 receiverId: userId,
                 isChecked: false,
             }
         });
-        return res.status(200).json(checkedCount);
+        return res.status(200).json(uncheckedCount);
     } catch (error: any) {
         next(error);
     }
